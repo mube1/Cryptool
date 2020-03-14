e0b055e5c2d162bf97771a72bd164102f5e9060c
mube1 <38358880+mube1@users.noreply.github.com>
2020-03-14 14:00:20 +0300

    Update Railfence Encryption Better.py

--- Railfence Encryption Better.py
+++ Railfence Encryption Better.py
@@ -6,9 +6,14 @@ offset=1 # the offset
 
 offset=offset%(2*keye-1) # offset at most can be twice the key minus 1
 
+ch='*'
+for i in range(33,47):
+    if Plaintext.find(ch):
+        break
+    ch=chr(i)
 
-
-Plaintext='*'*offset+Plaintext # populating the plaintext with a string of length ==offset
+Plaintext=ch*offset+Plaintext # populating the plaintext with a string made up of a character that
+# doesnt exist in the plaintext
 
 Cipher=['']*keye 
 
@@ -19,19 +24,22 @@ i=0
 
 while  i < Len:    
     Cipher[index]=Cipher[index]+ Plaintext[i]
-    if inc is True:
-        index=index+1
+    #just build the cipher with all the characters 
+
+    if inc is True: # if the path is up it increases else it decreases
+        index=index+1 
     else:
         index=index-1
 
-    if (index==keye-1):
+
+    if (index==keye-1): # if at the bottom, it starts goig up. if at up, going down
         inc=False
     if index==0:
         inc =True
     i=i+1
 
-for i in range(keye):
-    Cipher[i]=Cipher[i].replace('*','')
+for i in range(keye): # removing added characters from the build string
+    Cipher[i]=Cipher[i].replace(ch,'')
   
-print(''.join(Cipher))
+print(''.join(Cipher)) # join the separated strings
 print(Plaintext[offset:])
