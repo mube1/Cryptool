3e2b9443fbd9a702744ee58baff248b17cc15c63
mube1 <38358880+mube1@users.noreply.github.com>
2020-03-20 19:58:17 +0300

    Update Railfence Decryption Better.py

--- Railfence Decryption Better.py
+++ Railfence Decryption Better.py
@@ -1,30 +1,50 @@
-in="flaegkbdhjci"
-keye=4
-offset=1
+# the data used can be programmed to be from the user
+Cipher_text="flaegkbdhjci"
+keye=4 # the length of the key 
+offset=1 # the offset
+
+ch='*'
+
+    #looking for a character that doesnt exist in the plantext
+
+Len=len(Cipher_text)+ offset # total length assumed with offset in mind
 offset=offset%(2*keye-1) # offset at most can be twice the key minus 1
+Plain=[None]*(Len)
+G=2*(keye-1) # this is the distance between the two sharp edges of the fence
+E= keye-(offset%(keye-1))-1
+inc=0
 
-Input='*'*offset+Input
-Output=['']*keye
+for i in range(keye):
+    #Two cases are assumed:when the character is at the bottom or top of the fence and when it is not  
+    j=i
+    if j>0 and j<keye-1:
+        while j <Len:
+            if i==j and i <offset:
+                Plain[j]=ch
+                
+            else:
+                Plain[j]=(Cipher_text[inc])
+                inc=inc+1
+                
+            j=j+G-2*i
+            if j<Len:
+                if i>E and i==j-(G-2*i) and offset>keye:
+                    Plain[j]=ch
+                
+                else:
+                    Plain[j]=(Cipher_text[inc])
+                    inc=inc+1
+                
+                j=j+2*i
 
-Len=len(Input)
-index=0
-inc= True
-i=offset
-while  i < Len:
-    if i>keye or :
-        
     else:
-        Output[index]=Output[index]+ Input[i]
-    if inc is True:
-        index=index+1
-    else:
-        index=index-1
-
-    if (index==keye-1):
-        inc=False
-    if index==0:
-        inc =True
-    i=i+1
-    
-print (''.join(Output)[offset:])
+        while j < Len:
+            if i==j and i < offset:
+                Plain[j]=ch
+            else:
+                Plain[j]=(Cipher_text[inc])
+                inc=inc+1            
+            j=j+G
+            
+print(''.join(Plain).replace(ch,''))
 
